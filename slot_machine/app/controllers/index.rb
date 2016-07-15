d79de6f65c0e7fb2bb8944698d176549bca187eb
codeamac11 <codeamac11@gmail.com>
2016-07-14 20:30:16 -0500

    Slot machine resuleto

--- slot_machine/app/controllers/index.rb
+++ slot_machine/app/controllers/index.rb
@@ -14,6 +14,7 @@ post '/rolls' do
 
   @rolls = []
 
+
   if params[:value]
     3.times { @rolls << Roll.create({ value: value }) }
   else
--- slot_machine/public/js/application.js
+++ slot_machine/public/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+
+  $( "form" ).submit( function( event ) { 
+      
+    event.preventDefault();
+    
+    var $form = $( this ),
+    term = null
+    url = $form.attr( "action" );
+
+    $.post(url, term, function(data){
+       html = $(data).find("#die")
+       $( "#die" ).empty().append( html );
+
+    });
+
+  });
   
 });
