1d6792aba46e0d031b969c5ff0495e58d2951256
codeamac10 <codeamac10@gmail.com>
2016-07-19 20:14:40 -0500

    Terminado validaciones de formulario

--- valida_forma/application.js
+++ valida_forma/application.js
@@ -1,160 +1,48 @@
 $(document).ready(function() {
-  
-  tabla_completa(); //Esta función crea el tablero completo dinámicamente.
 
-  //Valores iniciales de tablero.
-  function valores_iniciales(){
-    counter = 0;
-    cancel_1 = 0;
-    valida1 = false;
-    valida2 = false;
-    $( '#start_btn' ).empty().append('Jugar')
-    $( '#Player1 td' ).removeClass('active');
-    $( '#Player2 td' ).removeClass('active');
-    $( '#Player1 td:nth-child(2)' ).addClass('active');
-    $( '#Player2 td:nth-child(2)' ).addClass('active');
-    $('.table #player1').css('background-color','')
-    $('.table #Player1 td').css('background-color','')
-    $('.table #Player2 td').css('background-color','')
-    $( '#left' ).empty().append('Presiona el botón para jugar')
-    $( '#right' ).empty()
-    timer = 5
-    count = 5
+  $( 'form' ).submit( function(event) {
 
-   }
+    event.preventDefault();
+    myFunction();
 
-  function countdown(){
-
-    var interval = setInterval(function() {
-    timer--;
-    $( '#left' ).empty().append(timer); 
-    if (timer == 0) {
-        // Display a login box
-        clearInterval(interval);
-        $( '#left' ).empty().append('Empieza'); 
-
-       }
-     }, 1000);
-    
-  }
-
-
-  // Función inicial de arrojar las monedas y reiniciar el juego (botón de Jugar/Reiniciar)
-  $( '#start_btn' ).click( function() {
-    
-      
-      if (counter == 0) { 
-        countdown();  
-        $(this).empty().append('Reiniciar'); 
-        setTimeout(function(){                     
-        my_var = setInterval(function(){ throw_coin_1() }, 10);
-        my_var_2 = setInterval(function(){ throw_coin_2() }, 10);
-        counter+=1
-        }, 5000);
-      
-      }
-      else {
-        valores_iniciales();
-      }
-   
   });
 
 
+ function myFunction() {
 
-  // Función para detener monedas.
-  function myStopFunction(x){
-    clearInterval(x);
+    var x, y, text;
+    x = document.getElementById("email").value;
+    y = document.getElementById("password").value;
 
-  }
-
-  // Función para generar tablero cpompleto.
-  function tabla_completa(){
-    $( '#Player1' ).each( function( key, el ) {
-      for ( var i = 0; i < 64; i++ ) { $(this).append('<td>' + '</td>'); };
-    });
-    $( '#Player2' ).each( function( key, el ) {
-      for ( var i = 0; i < 64; i++ ) { $(this).append('<td>' + '</td>'); };
-    });
-    valores_iniciales();
-  }
-
-  // Función para iniciar moneda 1.
-  function throw_coin_1(){
-
-    $(' #Player1 ').find(' td.active ').next().addClass("active");
-    $(' #Player1 ').find(' td.active ').first().removeClass("active");
-    if ($(' #Player1 ').find("td:nth-child(66)").attr("class") === "active")
-      { myStopFunction(my_var)
-        valida1 = true;
-        valida_juego();  
-        }
-  }
-
-  // Función para iniciar moneda 2.
-  function throw_coin_2(){
-    $(' #Player2 ').find(' td.active ').next().addClass("active");
-    $(' #Player2 ').find(' td.active ').first().removeClass("active");
-    if ($(' #Player2 ').find("td:nth-child(66)").attr("class") === "active")
-      { myStopFunction(my_var_2);
-        valida2 = true;
-        valida_juego();
-        }
-  }
-
-  function valida_juego(){
-    if ( valida1 == true && valida2 == true ){
-      winner();
-    }
-  }
-
-  function winner(){
-
-    res_1 = $(' #Player1 ').find(' td.active ').index();
-    res_2 = $(' #Player2 ').find(' td.active ').index();
-
-    if ( res_1 < 55 && res_2 < 55 ){
-      if ( res_1 > res_2 )
-        { $('.table #Player1 td').not('.active').css('background-color','#90EE90')
-          $('#right').empty().append('Ganador: ¡Jugador 1!')
-        }
-       if ( res_1 < res_2 )
-        { $('.table #Player2 td').not('.active').css('background-color','#90EE90')
-          $('#right').empty().append('Ganador: ¡Jugador 2!')}
-       if ( res_1 == res_2 )
-        {$('#right').empty().append('¡Empate!')}
-
+    if (x.length != 0 && validation_mail(x)) {
+        $("#mensaje_a").empty().append('<li>' + "Email OK" +'</li>');
     } else {
-      if (res_1 < 55 ){
-        $('.table #Player1 td').not('.active').css('background-color','#90EE90')
-        $('#right').empty().append('Ganador: ¡Jugador 1!')
-      }
-      else if (res_2 < 55 ){
-        $('.table #Player2 td').not('.active').css('background-color','#90EE90')
-        $('#right').empty().append('Ganador: ¡Jugador 2!')
-      } 
-      else {
-      $('#right').empty().append('¡Ambos pierden!')
-      }
+        $("#mensaje_a").empty().append('<li>' + "Debes escribir una dirección de email válida" +'</li>');
     }
-  }
 
-  $(document).keydown(function(e) {
-      if(e.keyCode == 81) {
-        i = true;
-        myStopFunction(my_var);
-        valida1 = true
-        valida_juego()
-        // counter += 1;
-      }
+    if (y.length != 0 && number_password(y) && y.length > 7 && capital_password(y)) {
+        $("#mensaje_b").empty().append('<li>' + "Password OK" +'</li>');
+    } else {
+        $("#mensaje_b").empty()
+        $("#mensaje_b").append('<li>' + "El password debe tener al menos un caracter numérico (0-9)" +'</li>');
+        $("#mensaje_b").append('<li>' + "El password debe contener al menos una mayúscula" +'</li>');
+        $("#mensaje_b").append('<li>' + "El password debe tener al menos 8 caracteres" +'</li>'); 
+    }
+}
 
-      if(e.keyCode == 80) {
-        i = true;
-        myStopFunction(my_var_2);
-        valida2 = true
-        valida_juego()
-        // valida_vars()
-        // counter += 1;
-      }
-    });
+function number_password(password){
+  var res = (/[0-9]/g).test(password);
+  return res
+}
+
+function capital_password(password){
+  var res = (/[A-Z]/g).test(password);
+  return res
+}
+
+function validation_mail(email) {
+  var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  var valid = re.test(email)
+  return valid
+}
 });
-
