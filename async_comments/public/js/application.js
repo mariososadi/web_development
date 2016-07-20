6651cc9406e09ae12a51dae7312963af833ce81f
codeamac11 <codeamac11@gmail.com>
2016-07-20 17:31:45 -0500

    Terminado comentarios asíncronos, falta darle formato a la forma....pero no hay tiempo por ahora

--- async_comments/public/js/application.js
+++ async_comments/public/js/application.js
@@ -1,10 +1,22 @@
 $( document ).ready(function() {
 
-$("#new_comment_button").click(function(){
-	
-	console.log("hola");
-	$("#comment_list").append("<li> Me gusta el post. No sé mucho acerca de programación pero me encantaría saber más.</li>");
+  $("#new_comment_button").click(function(){
+  	
+  	$("#new_comment_button").hide();
+  	$("#comment_list").append("<li> <form id=\"nuevo\" >Comentario:<br><input type=\"text\" id=\"comment\"><br>Nombre:<br><input type=\"text\" id=\"author\"><br><br><input type=\"submit\" value=\"Enviar\"></form></li>");
+    
 
-});
+    $("form").submit(function(event){
 
+      event.preventDefault();
+
+      var comment = document.getElementById("comment").value;
+      var author = document.getElementById("author").value;
+
+      $("#comment_list li:last").remove();
+      $("#comment_list").append("<li>" + comment  + "<span class=\"author\">" + author  + "</span></li>");  
+      $("#new_comment_button").show();
+
+    });
+  });
 });
