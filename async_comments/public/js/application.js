6f747b0109507615631ea2342be77b7602ff1625
mariososadi <mariososadi@gmail.com>
2016-07-20 01:53:34 -0500

    Agregué el ejercicio de comentarios asíncronos pero no lo he terminado

--- async_comments/public/js/application.js
+++ async_comments/public/js/application.js
@@ -1,3 +1,10 @@
-$(function(){
+$( document ).ready(function() {
+
+$("#new_comment_button").click(function(){
+	
+	console.log("hola");
+	$("#comment_list").append("<li> Me gusta el post. No sé mucho acerca de programación pero me encantaría saber más.</li>");
+
+});
 
 });
