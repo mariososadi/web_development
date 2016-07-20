35c0eb3610fd63c8cd6e7fc881cbf3eff658549c
codeamac10 <codeamac10@gmail.com>
2016-07-19 21:03:40 -0500

    Terminado dale_control_al_usuario

--- give_control_to_user/style.js
+++ give_control_to_user/style.js
@@ -1,3 +1,20 @@
-$(function(){
+$( '#button').click(function(event) {
+  event.preventDefault();
+   my_function();
 
 });
+
+function my_function() {
+
+      x = document.getElementById("selector").value;
+      y = document.getElementById("property").value;
+      z = document.getElementById("value").value;
+
+      console.log(x)
+      console.log(y)
+      console.log(z)
+
+      $(x).css(y, '');
+      $(x).css(y, z);
+
+};
