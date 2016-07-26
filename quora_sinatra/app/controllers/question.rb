8bb7a103f4184620c3596705405b338e915a9d44
codeamac10 <codeamac10@gmail.com>
2016-07-25 19:23:16 -0500

    Ahora si, todo el entorno de Users funciona perfectamente

--- quora_sinatra/app/controllers/question.rb
+++ quora_sinatra/app/controllers/question.rb
@@ -6,7 +6,6 @@ end
 
  get '/questions/new' do
   redirect_if_not_logged_in
-  @user = current_user
   erb :'/questions/new'
 end
 
--- quora_sinatra/app/controllers/user.rb
+++ quora_sinatra/app/controllers/user.rb
@@ -1,17 +1,35 @@
 get '/login' do
+
   @validations = params[:error]
   erb :'user/login'
+
 end
 
-get '/signup' do
-  @validations = params[:mess]
-  erb :'user/signup'
-  
-end
 
 post '/login' do
-  erb :'user/login'
+
+  if params[:email] == "" || params[:password] == ""
+    redirect to('/login?error=Please, introduce a valid email adress and password.')
+  else
+    @user = User.find_by(email: params[:email])
+    if User.authenticate(params[:email],params[:password])
+      session[:user_id] = @user.id
+      redirect '/questions' 
+    else
+      redirect to('/login?error=Incorrect email and password.')
+    end  
+  end
+
 end
+
+
+
+get '/signup' do
+
+  @validations = params[:mess]
+  erb :'user/signup'
+end
+
 
 post '/signup' do
    
@@ -21,13 +39,15 @@ post '/signup' do
       @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
       @user.password = params[:password]
       if @user.save
-        p session[:user_id] = @user.id
+        session[:user_id] = @user.id
         redirect '/questions' 
       else
         redirect to('/signup?mess=1')
       end  
     end
 end
+
+
 
 get '/logout' do
   if session[:user_id] != nil
